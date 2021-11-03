41b4e27a67c9e667ff485242f7e170891f57dd31
KIM DODAM <81546305+damdream@users.noreply.github.com>
2021-11-03 12:35:18 +0900

    Delete my_settings.py

--- my_settings.py
+++
@@ -1,9 +0,0 @@
-DATABASES = {
-    'default': {
-        'ENGINE': 'djongo',
-        'NAME': '1stProject',
-        'HOST': '127.0.0.1',
-        'PORT': 27017,
-    }
-}
-SECRET_KEY = 'django-insecure-ou_6mppx9w9t7qsu1x)fsmsg+1@dp48ce%yzs3rdz!5m5rirjx'
